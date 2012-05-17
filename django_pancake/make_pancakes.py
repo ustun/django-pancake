a95a6b7b532d70dae7884de1630a9ea398ba6b98
Ustun Ozgur <ustunozgur@gmail.com>
2012-05-17 11:42:54 +0300

    Ignore admin directory when flattening
    (workaround, not a real fix)

--- django_pancake/make_pancakes.py
+++ django_pancake/make_pancakes.py
@@ -6,7 +6,7 @@ def template_names(input_dir, prefix=''):
     for filename in os.listdir(input_dir):
         template_name = os.path.join(prefix, filename)
         full_name = os.path.join(input_dir, filename)
-        if os.path.isdir(full_name):
+        if os.path.isdir(full_name) and filename != "admin":
             for name in template_names(full_name, template_name):
                 yield name
         else:
@@ -15,6 +15,8 @@ def template_names(input_dir, prefix=''):
 def make_pancakes(input_dir, output_dir):
     templates = TemplateDirectory(input_dir)
     for template_name in template_names(input_dir):
+        if template_name == "admin":
+            continue
         print "Writing %s" % template_name
         pancake = flatten(template_name, templates)
         outfile = os.path.join(output_dir, template_name)
